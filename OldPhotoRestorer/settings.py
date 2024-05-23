11f1984f56c5ba0b19d2572be00ac78dc0ce2bb5
Marco Bertini <marco.bertini@unifi.it>
2024-05-23 15:42:34 +0200

    UPD: added all host to ALLOWED_HOST to ease deployment. Not needed when deploying on Heroku

--- OldPhotoRestorer/settings.py
+++ OldPhotoRestorer/settings.py
@@ -35,7 +35,7 @@ SECRET_KEY = django_key
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [HOST, 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['*', HOST, 'localhost', '127.0.0.1', '0.0.0.0']
 
 LOGGING = {
     'version': 1,
